82e3e14d3d83b87f42af666b39869c3783c4834c
William Sentosa <sentosa2@illinois.edu>
2020-08-19 14:24:09 -0500

    update

--- sender/one_way.cc
+++ sender/one_way.cc
@@ -49,10 +49,10 @@ int main( int argc, char *argv[] )
   const char * direction = argv[1];
   bool uplink = (strcmp(direction, "up") == 0);
 
-  sprintf(log_file_name,"%s-%s-%d-%d", server ? "server" : "client", uplink ? "up" : "down", (int)(ts/1e9),sender_id);
-  log_file=fopen(log_file_name,"w");
-
   bool is_send_data = (server && !uplink) || (!server && uplink);
+
+  sprintf(log_file_name,"%s-%s-%s-%d-%d", server ? "server" : "client", uplink ? "up" : "down", is_send_data ? "data" : "ack", (int)(ts/1e9),sender_id);
+  log_file=fopen(log_file_name,"w");
 
   SaturateServoOneWay saturatr( is_send_data ? "OUTGOING" : "INCOMING", log_file, data_socket, remote_data_address, server, sender_id );
 
