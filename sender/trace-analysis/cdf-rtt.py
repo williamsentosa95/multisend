384a6caf9fa7b2c10cd30ffbd44812754c1b39c7
William Sentosa <sentosa2@illinois.edu>
2021-01-01 00:40:26 -0600

    update

--- sender/trace-analysis/cdf-rtt.py
+++ sender/trace-analysis/cdf-rtt.py
@@ -4,7 +4,6 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-NUM_PACKET = 43000
 
 fig, ax = plt.subplots()
 fig.set_size_inches(20, 6)
@@ -39,16 +38,19 @@ def eliminate_redundant_missing(miss1, miss2):
 
 def get_rtts(fh):
   rtts = []
-  seqs = []
   for line in fh.readlines() :
-    if (len(rtts) < NUM_PACKET):
       fields = line.split();
       if (fields[0] == "INCOMING"):
         rtt = float(fields[7].split("=")[1].strip(",")) * 1000
-        seq_num = int(fields[4].split("=")[1].strip(","))
         rtts.append(rtt)
-        seqs.append(seq_num)
-  return rtts, seqs
+  return rtts
+
+def plot_cdf(rtt, i):
+  global plt
+  num_bins = len(rtt)
+  counts, bin_edges = np.histogram (rtt, bins=num_bins)
+  cdf = np.cumsum (counts)
+  plt.plot (bin_edges[1:], cdf/cdf[-1], label='RTT' + str(i))
 
 if(len(sys.argv) < 2 ) : 
   print("Usage : prep-for-simulation-fixed.py <num-trace> <trace-1> <trace-2> ...")
@@ -64,32 +66,27 @@ for i in range(2, 2+num_trace):
   fhs.append(open(file_name, "r"))
 
 for i in range(0, len(fhs)):
-  rtts, seqs = get_rtts(fhs[i])
-  all_rtts.append(rtts)
-  print("Missing seqs = " + str(missing_seq(seqs)))
+  all_rtts.append(get_rtts(fhs[i]))
   print("%d : RTTS min=%.2f 25th=%.2f 50th=%.2f 75th=%.2f 90th=%.2f 95th=%.2f 99th=%.2f max=%.2f" % (i, np.min(all_rtts[i]), np.percentile(all_rtts[i], 25), np.percentile(all_rtts[i], 50), np.percentile(all_rtts[i], 75), np.percentile(all_rtts[i], 90), np.percentile(all_rtts[i], 95), np.percentile(all_rtts[i], 99), np.max(all_rtts[i])))
 
 
 for i in range(0, len(fhs)):
-  ax.plot(all_rtts[i], label='RTT' + str(i))
+  plot_cdf(all_rtts[i], i)
 
 SMALL_SIZE = 8
 MEDIUM_SIZE = 15
 BIGGER_SIZE = 20
 
-
-
-ax.set_ylabel('Round Trip Time (ms)', fontsize=BIGGER_SIZE)
-ax.set_xlabel('Packet index', fontsize=BIGGER_SIZE)
+ax.set_ylabel('CDF', fontsize=BIGGER_SIZE)
+ax.set_xlabel('RTT (ms)', fontsize=BIGGER_SIZE)
 ax.legend()
 plt.grid(True)
 # ax.set_xticks(result_time_up, True)
-# ax.set_ylim(0, 100)
-# plt.tight_layout()
+ax.set_xlim(0, 200)
+#plt.tight_layout()
 
 plt.xticks(fontsize=BIGGER_SIZE)
 plt.yticks(fontsize=BIGGER_SIZE)
 plt.legend(fontsize=MEDIUM_SIZE)
-# plt.xlabel(fontsize=BIGGER_SIZE)
 
 plt.show()
